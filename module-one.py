ff30f38e71530b05df99f78d24ac878d17674bf0
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2023-09-01 21:12:42 +0200

    Klart.

---
+++ module-one.py
@@ -0,0 +1,44 @@
+# upp.1
+print("hello world")
+
+
+# upp.2
+print("hello \"world\"")
+
+
+# upp.3
+print("""
+Lorem ipsum
+Lorem ipsum
+Lorem ipsum
+Lorem ipsum
+Lorem ipsum
+Lorem ipsum
+Lorem ipsum
+Lorem ipsum
+Lorem ipsum
+Lorem ipsum
+""")
+
+
+# upp.4
+print("one\ntwo\nthree !")
+
+
+# upp.5 
+print("hello " + "world")
+
+
+# upp.6
+name = "Daniil"
+print("hello ", name)
+
+
+# upp.7
+yourNumber = input("enter your number : ")
+print("your number is... %s !" % (yourNumber))
+
+
+# upp.8
+userName = input("whats your name ? : ")
+print("welcome %s !" %(userName))
